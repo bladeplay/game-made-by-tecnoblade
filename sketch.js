db37f466fdd170a96704564facc12f9198dc5d7d
bladeplay <68857883+bladeplay@users.noreply.github.com>
2020-09-10 19:41:31 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+var canvas;
+var gameState=0;
+var playerCount;
+var form,player,game;
+var database,position;
+function setup(){
+    database=firebase.database();
+
+    canvas=createCanvas(400,400);
+
+    game = new Game();
+     game.getState();
+      game.start();
+
+}
+
+function draw(){
+
+}
